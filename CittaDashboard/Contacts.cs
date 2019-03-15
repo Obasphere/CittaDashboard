c320e6ac83b5edd9b60591ea3fab408f7ff5c7f0
obasphere <dami.d.adesanya@gmail.com>
2019-03-15 09:47:38 +0100

    Added CittaDashboard/Contacts.cs

---
+++ CittaDashboard/Contacts.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CittaDashboard
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public partial class Contacts
+    {
+        public int TableId { get; set; }
+        [Display(Name = "Add Comments")]
+        public string Name { get; set; }
+        public Nullable<System.DateTime> Created_at { get; set; }
+    }
+}
