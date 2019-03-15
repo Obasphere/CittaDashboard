9d74c3c2ace06f93234de6a6d1f50e27a53e2eaf
obasphere <dami.d.adesanya@gmail.com>
2019-03-15 09:46:31 +0100

    Added  CittaDashboard/Controllers/AddController.cs

---
+++ CittaDashboard/Controllers/AddController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CittaDashboard.Models.DB;
+
+namespace CittaDashboard.Controllers
+{
+    public class AddController : Controller
+    {
+        // GET: Add
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(CittaDashboard.Models.DB.Contact model)
+        {
+            Entities entity = new Entities();
+            model.Created_at = DateTime.Now;
+            entity.Contacts.Add(model);
+            entity.SaveChanges();
+            return View();
+        }
+    }
+}
